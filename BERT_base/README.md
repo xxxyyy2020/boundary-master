666ae59848988dcac265cc6ef605d04b2be5e518
xxxyyy2020 <77620172+xxxyyy2020@users.noreply.github.com>
2021-03-07 22:17:52 +0800

    Delete BERT_base directory

--- BERT_base/README.md
+++
@@ -1,1 +0,0 @@
-# train the models which based on bert
--- BERT_base/stimulate_data_raw/__init__.py
+++
@@ -1,7 +0,0 @@
-#-*- coding: = utf-8 -*-
-# author = lxj 
-# date = 2020/3/2
-"""
-存在的另一个论文的数据集　Detecting Emotion Stimuli in Emotion-Bearing Sentences
-是关于span的数据集
-"""
